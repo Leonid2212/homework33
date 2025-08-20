e7e1af6dcf71b7a158b3bfcfa99861c6b7af3186
Leonid <Lstrelcov276@gmail.com>
2025-08-20 17:53:04 +0300

    hw33

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,95 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        byte b = 67;
+        short c = 569;
+        int a = 27897;
+        long d = 987678965549L;
+        float e = 2.786f;
+        double f = 27.12;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(a);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+
+
+        byte lp = 23;
+        byte pa = 27;
+        byte ea = 30;
+        short bm = 480;
+        System.out.println(lp + pa + ea);
+        System.out.println(bm/80);
+
+
+        byte bot = 16;
+        byte min = 2;
+        System.out.println(bot / min);
+        System.out.println( 8 * 20 );
+        System.out.println(1440 * 8 );
+        System.out.println(11520 * 3 );
+        System.out.println(11520 * 30 );
+
+
+        byte ban = 120;
+        byte whi = 2;
+        byte bro = 4;
+        System.out.println(whi + bro);
+        System.out.println(ban / 6);
+        System.out.println(20 * whi);
+        System.out.println(20*4);
+
+        byte banana = 5;
+        short milk = 200;
+        byte  icecream = 2;
+        byte  egg = 4;
+
+        System.out.println(banana * 80);
+        System.out.println(milk = 105 * 2);
+        System.out.println(icecream * 100);
+        System.out.println(egg * 70);
+        System.out.println(400+210+200+280);
+        System.out.println( 1090 / 1000);
+
+
+        byte kg = 7;
+        short mg1 = 250;
+        short mg2 = 500;
+        System.out.println(kg*1000);
+        System.out.println(7000/mg1);
+        System.out.println(7000/mg2);
+        System.out.println(28+14/2 );
+
+
+        int m = 67760;
+        int den = 83690;
+        int k = 76230;
+
+        System.out.println( m * 1.1 );
+        System.out.println(den * 1.1 );
+        System.out.println (k * 1.1 );
+        System.out.println(74536- m );
+        System.out.println(92059 - den );
+        System.out.println(83853 - k );
+        System.out.println(m * 12);
+        System.out.println(den * 12);
+        System.out.println(k * 12);
+        System.out.println(74536 * 12);
+        System.out.println(92059 *12 );
+        System.out.println(83853 *12 );
+        System.out.println(894432 - 813120 );
+        System.out.println(1104708 - 1004280 );
+        System.out.println(1006236 - 914760);
+
+
+
+
+
+
+
+
     }
 }
+
